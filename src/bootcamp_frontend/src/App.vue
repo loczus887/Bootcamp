ab8c6e96dc3cc16578f0d8a5618ed5ba0db6c11c
loczus887 <alakowalska2000@wp.pl>
2024-06-24 23:46:42 +0200

    Files

---
+++ src/bootcamp_frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    Siema!
+  <br/>
+    {{ licznik }}
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      licznik: 0
+    }
+  },
+}
+</script>
